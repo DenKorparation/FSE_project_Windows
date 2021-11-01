b6a7b3855a556ae0667012c0c42901f377dfd413
_DEN_ <kononchukd04@gmail.com>
2021-11-02 00:23:39 +0300

    add request for Current Weather Data

--- src/main/java/com/uni/Database.java
+++ src/main/java/com/uni/Database.java
@@ -5,11 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import org.json.JSONObject;
 
 public class Database {
+    private DataOfWeather curWeatherData = new DataOfWeather();
+    //private DataOfWeather[] hourlyForecast = new DataOfWeather[48];
+    //private DataOfWeather[] dailyForecast = new DataOfWeather[7];
+
     public void request(String nameOfCity)
     {
-        System.out.println(getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + nameOfCity + "&appid=c76548e17d6b42b99e631401cd0e0f75"));
+        reqCurWeather("https://api.openweathermap.org/data/2.5/weather?q=" + nameOfCity + "&units=metric&appid=c76548e17d6b42b99e631401cd0e0f75");
+    }
+    private void reqCurWeather(String url){
+        String output = getUrlContent(url);
+        if(!output.isEmpty()){
+            JSONObject obj = new JSONObject(output);
+            curWeatherData.setTemp(obj.getJSONObject("main").getFloat("temp"));
+            curWeatherData.setFeelsLikeTemp(obj.getJSONObject("main").getFloat("feels_like"));
+            curWeatherData.setWindSpeed(obj.getJSONObject("wind").getFloat("speed"));
+            curWeatherData.setPressure(obj.getJSONObject("main").getFloat("pressure"));
+            curWeatherData.setHumidity(obj.getJSONObject("main").getFloat("humidity"));
+        }
     }
 
     private static String getUrlContent(String urlAdress) {
@@ -28,5 +44,9 @@ public class Database {
             System.out.println("Такой город не был найден!");
         }
         return content.toString();
+    }
+
+    public DataOfWeather getCurWeatherData() {
+        return curWeatherData;
     }
 }
