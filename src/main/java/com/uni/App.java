78e439bc62fa198e8828c7ac4421f9534abec8a0
_DEN_ <kononchukd04@gmail.com>
2021-11-23 00:56:01 +0300

    added custom list

--- src/main/java/com/uni/App.java
+++ src/main/java/com/uni/App.java
@@ -1,6 +1,7 @@
 package com.uni;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,15 +13,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class App extends Application {
-
+    AppController mainSceneController = new AppController();
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/App.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/App.fxml"));
+        Parent root = loader.load();
+        mainSceneController = loader.getController();
         primaryStage.setTitle("Gismeteostats");
         primaryStage.setScene(new Scene(root, 1100 ,  650));
         primaryStage.setResizable(false);
         primaryStage.getIcons().add(new Image("https://ds-blobs-2.cdn.devapps.ru/6380085.png"));
         primaryStage.show();
+        mainSceneController.postInit();
     }
 
 
--- src/main/java/com/uni/AppController.java
+++ src/main/java/com/uni/AppController.java
@@ -2,6 +2,7 @@ package com.uni;
 
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Objects;
@@ -9,91 +10,72 @@ import java.util.ResourceBundle;
 import java.util.TimeZone;
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 
 public class AppController {
-
-    private Database database = new Database();
-
+    public static Database database = new Database();
     @FXML
     private ResourceBundle resources;
-
     @FXML
     private URL location;
-
     @FXML
     private Button EnterButton;
-
     @FXML
     private TextField EnterCity;
-
     @FXML
     private Label Temp;
-
     @FXML
     private Label feelslikeTemp;
-
     @FXML
     private Label humidity;
-
     @FXML
     private Label lbl;
-
     @FXML
     private Label curTime;
-
     @FXML
     private Label pressure;
-
     @FXML
     private Label PartOfDay;
-
     @FXML
     private ImageView mainIm;
-
     @FXML
     private Label condition;
-
     @FXML
     private Label windSpeed;
-
     @FXML
     private String curCond;
-
     @FXML
     private String partOfDay;
-
     @FXML
     private Label result_info;
-
     @FXML
     private ImageView humidityIm;
-
     @FXML
     private ImageView windIm;
-
-    @FXML
-    private Label hourlyForecast;
-
-
     @FXML
     private ImageView pressureIm;
     @FXML
     private ImageView mainIcon;
     @FXML
     private ImageView magnifier;
-
     @FXML
-    private ScrollPane hForecast;
-
+    private AnchorPane hourlyWeatherList;
     @FXML
     private ScrollBar hForecastScroll;
 
+    private Node[] nodeHourly = new Node[48];
+    private  ItemController[] nodesHourlyController = new ItemController[48];
     @FXML
     void OnClickMethod() {
         database.setNameOfCity(EnterCity.getText());
@@ -164,15 +146,10 @@ public class AppController {
                         mainIm.setImage(new Image("thunder.jpg"));
                         mainIcon.setImage(new Image("11d.png"));
                     }
-                    hForecastScroll.valueProperty().addListener(event -> {
-                        for (int i=0; i<48; i++) {
-                            float hCast;
-                            hCast = database.getHourlyForecast()[i].getTemp();
 
-
-                        }
-                    });
-
+                    for (int i = 0; i < nodeHourly.length; i++){
+                        nodesHourlyController[i].update(i);
+                    }
 
                     result_info.setText("");
                 }
@@ -188,6 +165,39 @@ public class AppController {
 
     @FXML
     void initialize() {
+        for (int i=0; i<48; i++) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/item.fxml"));
+                nodeHourly[i] = loader.load();
+                nodesHourlyController[i] = loader.getController();
+                hourlyWeatherList.getChildren().add(nodeHourly[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
+
+        hForecastScroll.setMin(0);
+        hForecastScroll.setMax(1);
+        hForecastScroll.setValue(0);
+        hForecastScroll.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                for (int i = 0; i < nodeHourly.length; i++) {
+                    double node_width = nodeHourly[i].getLayoutBounds().getWidth();
+                    nodeHourly[i].setTranslateX(node_width * 1.2 * i - (node_width * 1.2 * nodeHourly.length - 0.2 * node_width - hourlyWeatherList.getLayoutBounds().getWidth()) * hForecastScroll.getValue());
+                }
+            }
+        });
+    }
+
+    public void postInit(){
+        for(int i = 0; i < nodeHourly.length; i++) {
+            nodeHourly[i].setTranslateX(nodeHourly[i].getLayoutBounds().getWidth() * 1.2 * i);
+            AnchorPane.setTopAnchor(nodeHourly[i], (hourlyWeatherList.getHeight() - nodeHourly[i].getLayoutBounds().getHeight()) / 2);
+            System.out.println(hourlyWeatherList.getHeight() - nodeHourly[i].getLayoutBounds().getHeight() / 2);
+        }
+        Rectangle clip = new Rectangle(hourlyWeatherList.getWidth(), hourlyWeatherList.getHeight());
+        hourlyWeatherList.setClip(clip);
     }
 }
